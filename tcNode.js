80a909c88fe26dc9ea1a447755d2252615e25b41
lixianghui <48141930+lxhNB@users.noreply.github.com>
2024-03-21 17:35:40 +0800

    增加一下2.x压缩纹理工具注释信息

--- tcNode.js
+++ tcNode.js
@@ -19,8 +19,11 @@ const _etcSettings = {
         ]
       }
 };
-let sourcePath = process.argv[2];
-let isCompress = parseInt(process.argv[3]);
+// 0 1 分别为nodejs的exe绝对路径  当前js文件所在绝对路径
+let sourcePath = process.argv[2]; //项目路径 自己传进来
+let isCompress = parseInt(process.argv[3]);//字符串  命令传进来的
+// console.log('打印文件',process)
+console.log("当前目录的全路径",__dirname) // 当前目录的全路径 E:\cocosProject\uiFramework2
 
 let lookupDir = function(url) {
     if (!fs.existsSync(url)) {
@@ -32,9 +35,12 @@ let lookupDir = function(url) {
             return;
         }
         files.forEach((file) => {
+            // console.log("打印每个文件的字符串",file)   //只是文件名加后缀
             let curPath = path.join(url, file);
-            let stat = fs.statSync(curPath);
-            if (stat.isDirectory()) {
+            // fs.stat(curPath, (err, stats) => {
+            // });
+            let stat = fs.statSync(curPath); //返回文件信息状态的实例
+            if (stat.isDirectory()) { //判断当前文件是不是目录
                 lookupDir(curPath); // 遍历目录
             } else {
                 if (file.indexOf('.meta') >= 0) {
@@ -44,6 +50,7 @@ let lookupDir = function(url) {
                             return;
                         }
                         let obj = JSON.parse(data);
+                        console.log('遍历到的meta文件对应的data',data,obj)
                         if (obj && obj.platformSettings) {
                             obj.platformSettings = isCompress ? _etcSettings : {}; // 设置压缩纹理
                             let wrdata = JSON.stringify(obj, null, 2);
